8f7d10a3acbdcb9ad845e024d2b574726b032043
wakuwakuJINI <synthi6785@gmail.com>
2024-08-19 21:51:58 +0900

    Add files via upload

---
+++ recipe.js
@@ -0,0 +1,41 @@
+// 레시피 세부 정보를 현재 페이지에 표시
+function displayRecipePage(meal) {
+  // 특정 레시피의 세부 정보를 가져오는 함수
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${meal.idMeal}`)
+  .then(response => response.json())
+  .then(data => {
+    const detailedMeal = data.meals[0]; // 상세 정보 가져오기
+    const contentDiv = document.getElementById('content');
+    contentDiv.innerHTML = `
+          <div class="recipe-container">
+                    <div class="left-container">
+                        <h1>${detailedMeal.strMeal}</h1>
+                        <img src="${detailedMeal.strMealThumb}" alt="${detailedMeal.strMeal}">
+                    </div>
+                    <div class="right-container">
+                        <h3>Ingredients</h3>
+                        <ul class="ingredients-list">
+                            ${getIngredientsWithMeasures(detailedMeal).map(item => `<li>${item}</li>`).join('')}
+                        </ul>
+                    </div>
+                    <div class="instruction-container">
+                        <h3>Instructions</h3>
+                        <p>${detailedMeal.strInstructions}</p>
+                    </div>
+                    </div>
+            `;
+    }
+)}
+
+// 재료와 과정, 가져오기
+function getIngredientsWithMeasures(meal) {
+    const ingredients = [];
+    for (let i = 1; i <= 20; i++) {
+        const ingredient = meal[`strIngredient${i}`];
+        const measure = meal[`strMeasure${i}`];
+        if (ingredient) {
+            ingredients.push(`${ingredient} - ${measure}`);
+        }
+    }
+    return ingredients;
+}
